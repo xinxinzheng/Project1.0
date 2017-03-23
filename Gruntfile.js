0f37e3f8a03d15fb2acd900491c48e6e3d4cab3c
zhengxinxin <zhengxinxin@360.cn>
2017-03-23 14:00:53 +0800

    all

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+
+  module.exports = function(grunt){
+
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+
+		jshint:{
+			files:['Gruntfile.js','js/*.js'],
+		},
+
+		csslint:{
+			options:{},
+			files:['stylesheet/*.css'],
+		},
+		
+        concat: {
+            options: {
+                process: function(src, filepath) {
+                    if (filepath.substr(filepath.length - 2) === 'js') {
+                        return '// Source: ' + filepath + '\n' +
+                            src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+                    } else {
+                        return src;
+                    }
+                }
+            },
+    		js: {
+      			src: [
+                    ,'js/controller/*.js'
+                ],
+      			dest: 'js/index.js',
+    		},
+  		},
+
+        watch: {
+            js: {
+                files: ['js/*/*.js','Gruntfile.js'],
+                tasks: ['concat:js','jshint'],
+            },
+            html: {
+                files: ['temp/*.html'],
+            },
+        },
+
+  		connect: {
+            dev: {
+                options: {
+                    port: 8081,
+                    base: ['js','temp','node_modules']
+                }
+            },
+        },
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default',['connect','concat','watch']);
+
+    
+  };
+
+
+
+
