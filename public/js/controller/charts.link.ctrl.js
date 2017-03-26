6192b90b33f37731707c81d6a6992469b7d3ff08
郑新新 <zhengxinxin@zhengxinxindeMacBook-Pro.local>
2017-03-26 19:59:38 +0800

    all

--- public/js/controller/charts.link.ctrl.js
+++ public/js/controller/charts.link.ctrl.js
@@ -6,7 +6,45 @@
  */
 "use strict";
 app.controller('chartlinkCtrl',function($rootScope,$scope){
+$scope.conf = {};
+$scope.getData = function(){
+	$http({
+		url:'nodes.json',
+		type:'GET'
+	})
+}
+$scope.conf.link = [
+{'src':'openflow:1','desc':'openflow:2 '},
+{'src':'openflow:1','desc':'openflow:3 '},
+{'src':'openflow:2','desc':'openflow:4 '},
+{'src':'openflow:2','desc':'openflow:3 '}
+];
+$scope.getLinkSpeed = function(){
+	$scope.getData().success(function(pre){
+		var pre = pre.nodes;
+		$setTimeout(function(){
+			$scope.getData().success(function(next){
+				var next = next.nodes;
+			})
+		}, 1);
+	})
 
+}
+$scope.produceCharts = function(){
+	var charts = [];
+	$scope.conf.link.map(function(_item,_i){
+		var data = [], time = (new Date()).getTime();  
+		for(var i = -19 ; i < 0 ; i++){
+			data.push({x: time + i * 5000,y: Math.random()})
+		}                                                        																				                                                                                                                                                    
+		var chart = {                                                              
+			name: _item.src + ' ' + _item.desc ,                                                
+			data: data                                                               
+		};
+		charts.push(chart);
+	});console.log(charts);
+	return charts;
+}
 $scope.initChartslink = function(){
 	Highcharts.setOptions({                                                     
 		global: {                                                               
@@ -19,29 +57,30 @@ $scope.initChartslink = function(){
 			animation:true,                                                             
 			events: {                                                           
 				load: function() {                                              																				            
-					var series = this.series[0] , serics1 = this.series[0];                                
-					setInterval(function() {                                    
-						var x = (new Date()).getTime(),         
-							y = Math.random();                                  
-						series.addPoint([x, y], true, true);   
-						// serics1.addPoint([x, y], true, true);                  
-					}, 1000);                                                   
+					var series = this.series;                                
+					series.map(function(_item,_i){
+						setInterval(function() {                                    
+							var x = (new Date()).getTime(),         
+								y = Math.random();                                  
+							_item.addPoint([x, y], true, true);                  
+						}, 5000);  
+					})					                                                 
 				}                                                               
 			},
 			style:{'border-radius':'0 0 5px 5px','width':'100%','height':400 }
 			                                                                 
 		},                                                                      
-		// title: {                                                                
-		// 	text: 'CPU动态走势图--1秒'                                            
-		// },                                                                      
+		title: {                                                                
+				text: '链路实时速率--5秒'                                            
+		},                                                                      
 		xAxis: {                                                                
 			type: 'datetime',                                                   
-			tickPixelInterval: 100                                              
+			tickPixelInterval: 100                                             
 		},                                                                      
 		yAxis: {                                                                
-			// title: {                                                            
-			// 	text: 'CPU动态走势图--1秒'                                                   
-			// },                                                                  
+			title: {                                                            
+				text: '速率'                                                   
+			},                                                                  
 			plotLines: [{                                                       
 				value: 0,                                                       
 				width: 1,                                                       
@@ -50,8 +89,8 @@ $scope.initChartslink = function(){
 		},                                                                      
 		tooltip: {                                                              
 			formatter: function() {                                             
-					return '<b>'+ this.series.name +'</b><br/>'+                
-					Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+					return '<b> 链路：'+ this.series.name +'</b><br/>时间：'+                
+					Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>速率：'+
 					Highcharts.numberFormat(this.y, 2);                         
 			}                                                                   
 		},                                                                      
@@ -61,23 +100,7 @@ $scope.initChartslink = function(){
 		exporting: {                                                            
 			enabled: true                                                      
 		},                                                                      
-		series: [{                                                              
-			name: 'Random data',                                                
-			data: (function() {                                                 
-				// generate an array of random data                             
-				var data = [],                                                  
-					time = (new Date()).getTime(),                              
-					i;                                                          
-																				
-				for (i = -19; i <= 0; i++) {                                    
-					data.push({                                                 
-						x: time + i * 1000,                                     
-						y: Math.random()                                        
-					});                                                         
-				}                                                               
-				return data;                                                    
-			})()                                                                
-		}]                                                                      
+		series: $scope.produceCharts()                                                                      
 	});  
 }
 $scope.initChartslink();
